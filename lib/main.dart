35b98ba91ba10cc64ebe0384d218565d0114d81a
ElisamaCalva <elisamaarturo.calvamoreno@utxicotepec.edu.mx>
2023-09-21 22:33:15 -0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:tarea5/src/button.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MyButton(),
+  ));
+}
